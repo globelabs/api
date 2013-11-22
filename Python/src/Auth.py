91ef50358e445642188eb491ac79ae75c253779b
clark galgo <clark@linux-41wx.site>
2013-11-22 15:27:18 +0800

    rename Oauth to Auth

---
+++ Python/src/Auth.py
@@ -0,0 +1,26 @@
+import urllib
+import requests
+import Base
+
+class Auth(Base.Base):
+	def __init__(self, key, secret):
+		super(Auth, self).__init__()
+
+		self.key = key
+		self.secret = secret
+
+		self.url = {}
+		self.url['login'] = 'http://developer.globelabs.com.ph/dialog/oauth?app_id=%s'
+		self.url['token'] = 'http://developer.globelabs.com.ph/oauth/access_token?app_id=%s&app_secret=%s&code=%s'
+
+	def getLoginUrl(self):
+		""" Get the Oauth login url"""
+
+		return self.url['login'] % self.key
+
+	def getAccessToken(self, code):
+		""" Request for access token """
+
+		url = self.url['token'] % (self.key, self.secret, code)
+		return self.getResponse(url, 'post')
+
