3a48cb3fc9bc4398eb622ab15a24260cd5b93d22
danmolina <danmichaelmolina@gmail.com>
2013-11-22 20:59:15 +0800

    Update receive.rb

--- Ruby/test/receive.rb
+++ Ruby/test/receive.rb
@@ -1,15 +1,18 @@
+# In this example, we use rack to do a HTTP.
+# You can use whatever library you want to handle HTTP Requests
 require 'rack'
+# We require also json for encoding the data
 require 'json'
 
 class Receive
-	
+    
     def call(env)
         result = String.new
 		
         req = Rack::Request.new(env)
 		
         # This is a catch for data that globe sends
-        # whenever the subscriber replied
+        # whenever the subscriber replied to your app (shortcode)
         if req.post?
             esult = req.POST()
         end
@@ -29,6 +32,12 @@ class Receive
 	
 end
 
+# Let's create an HTTP to handle the POST data
+# Here in this exampl, we use the same method 
+# in getting access token and receive message
+# we just separate it my method
+# Globe is using GET method to send the access token
+# while POST method is being used for receiving SMS
 Rack::Handler::WEBrick.run(
     Receive.new,
     :Port => 9000
