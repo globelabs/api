93a40738ef63fd7b1d51b6ce3abca7244a8076f8
danmolina <openovatelabs@Openovates-MacBook-Pro.local>
2013-11-22 20:43:26 +0800

    updated the test script

--- Ruby/test/test.rb
+++ Ruby/test/test.rb
@@ -1,38 +1,22 @@
 require './../src/GlobeApi.rb'
-require './../src/Sms.rb'
-require 'rack'
-
-appId = 'MeBKgt9nRb8tBginqMTR6gtEaB8BtAb7'
-appSecret = '149900c1de054210631719796413d2b0d5d723d8fefed1dda9cac21495a09054'
-code = 'k95U8AraXfejdx6Hg6oy8Sae46GHa4yqMtzXaB5SqLR8aIL54goUq5RBGSGg49bFBd5kpUM8Rj6ugxeoptLxer9CLAjxXFLEG5BCLprRAsXkb4rh6kqyjsnE97jF5bcbXGi9yAFzMqoxs47bMGh9Xr6EsoRGo9CdqjeBFpdejLCaLeKkt4ARL7uBM58AUj5465F6kRG7SMe4nMU9pREaI4GaGXSbAye4tBR4rxH4joXjSk4dnzHBnro5f6BkaxUA'
-accessToken = 'q3JYrRuW_4_KglWC20iRmk5qNaK3_-o1xdwipACY6dA'
-shortcode = '3651'
-subscriber = '09175646847'
-
 
 # Initialize GlobeApi Class
 globe = GlobeApi.new()
 
 ###### Authentication #######
-auth = globe.auth(appId, appSecret)
+auth = globe.auth([APP_ID], APP_SECRET)
 
 # Get Login URL
-#puts auth.getLoginUrl
+puts auth.getLoginUrl
 
 # Get Access Token
-#puts auth.getAccessToken(code)
+puts auth.getAccessToken([CODE])
 
 ###### SMS ######
 # Send SMS (Outbound)
-#puts globe.sms(shortcode)
-#	.sendMessage(accessToken, subscriber, 'hello')
+puts globe.sms([SHORTCODE])
+	.sendMessage([YOUR_ACCESS_TOKEN], [SUBSCRIBER_NUMBER], [MESSAGE])
 
 # Charge Subscriber
-puts globe.payment(accessToken, subscriber)
-	.charge('0.00', '36511000009')
-
-data = '{"test": "dan"}'
-
-response = globe.sms(5416).getMessage(data)
-puts response['test']
-#puts 'da'
+puts globe.payment([YOUR_ACCESS_TOKEN], [SUBSCRIBER_NUMBER])
+	.charge([AMOUNT], [REFERENCE_NUMBER])
