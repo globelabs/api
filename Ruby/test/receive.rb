ec3e5415eee8f4a77d52d6087c3d1507782c4b84
danmolina <openovatelabs@Openovates-MacBook-Pro.local>
2013-11-22 20:35:15 +0800

    updated the sms class and add a receive sample

---
+++ Ruby/test/receive.rb
@@ -0,0 +1,34 @@
+require 'rack'
+require 'json'
+
+class Receive
+	def call(env)
+		result = String.new
+		
+		req = Rack::Request.new(env)
+		
+		# This is a catch for data that globe sends
+		# whenever the subscriber replied
+		if req.post?
+		  result = req.POST()
+		end
+		
+		# This is a catch for data coming from
+		# requesting access token via SMS
+		if req.get?
+		  result = req.GET()
+		end
+		
+		return [
+			200,
+			{'Content-Type' => 'text/html'},
+			[JSON.generate(result, quirks_mode: true)]
+		]
+	end
+	
+end
+
+Rack::Handler::WEBrick.run(
+	Receive.new,
+	:Port => 9000
+)
