15e480f33067fbd527f8c64da628f8ebfa0a0898
adin234 <adinbautista@gmail.com>
2013-11-22 21:44:53 -0800

    fixed charge class

--- PHP/src/Payment.php
+++ PHP/src/Payment.php
@@ -29,14 +29,12 @@ class Payment extends GlobeAPI
      * @param string|null $accessToken      the access token of the user to be charged
      */
     public function __construct(
-        $shortCode = null, 
         $version = null,
         $endUserId = null,
         $accessToken = null
     ) {
 
         $this->version = $version;
-        $this->shortCode = $shortCode;
         $this->endUserId = $endUserId;
         $this->accessToken = $accessToken;
         $this->transactionOperationStatus = 'charged';
@@ -63,7 +61,7 @@ class Payment extends GlobeAPI
      * @return array 
      */
     public function charge($amount=null, $refNo=null, $bodyOnly = true) {
-        if($amount!=null) {
+        if($amount!==null) {
             $this->setAmount($amount);
         }
 
