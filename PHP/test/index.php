9fe46c81fc02d6939aefd767bb2ec3a91bba081c
adin234 <adinbautista@gmail.com>
2013-11-22 21:46:55 -0800

    updated test

--- PHP/test/index.php
+++ PHP/test/index.php
@@ -1,52 +1,34 @@
 <?php
-    include('PHP/GlobeApi.php');
-
     session_start();
-
-    $globe = new GlobeApi('v1');
-
-    $oAuth = $globe->oAuth(
-        'app_key',
-        'app_secret'
+    require ('api/PHP/src/GlobeApi.php');
+    $globe = new GlobeApi();
+    $auth = $globe->auth(
+        [KEY],
+        [SECRET]
     );
-
-    if(!isset($_SESSION['code']) && !isset($_SESSION['access_token'])) {
-        header(
-            'Location: '.
-            $oAuth->getAuthUrl()
-        );
+    
+    if(!isset($_SESSION['code'])) {
+        $loginUrl = $auth->getLoginUrl();
+        header('Location: '.$loginUrl); 
+        exit;
     }
-
+    
     if(!isset($_SESSION['access_token'])) {
-        $response = $oAuth->getAccessToken($_SESSION['code']);
-
+        $response = $auth->getAccessToken($_SESSION['code']);
         $_SESSION['access_token'] = $response['access_token'];
         $_SESSION['subscriber_number'] = $response['subscriber_number'];
-
     }
 
     $sms = $globe->sms(5286);
-    $outbound = $sms->setMessage('adin ang pogi mo')
-        ->setRecepient($_SESSION['subscriber_number']);
-    $outbound->setAccessToken($_SESSION['access_token']);
-
-    $response = $outbound->send();
-    echo 'send response: ';print_r($response);echo '<br/>';    
-
-    $response = $outbound->send(
-        $_SESSION['access_token'],
-        $_SESSION['subscriber_number'],
-        'this message is sent alone'
-    );
-    echo 'send response: ';print_r($response);echo '<br/>';    
-
+    $response = $sms->sendMessage($_SESSION['access_token'], $_SESSION['subscriber_number'], 'pangit po si sherwin');
 
     $charge = $globe->payment(
         $_SESSION['access_token'],
         $_SESSION['subscriber_number']
     );
 
-    //$response = $charge->charge('0', 'asdfasdfasdfasdf123123');
-    echo 'charge response: ';print_r($response);echo '<br/>';
-
+    $response = $charge->charge(
+        0,
+        '52861000001'
+    );
 ?>
