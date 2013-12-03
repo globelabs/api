4faecd2c9a118403a05ca8f074b6ad7fcb3505bb
clark galgo <clark@linux-41wx.site>
2013-12-03 17:10:46 +0800

    Add getMessage function for retrieving messages

--- Python/test/index.py
+++ Python/test/index.py
@@ -9,23 +9,29 @@ import GlobeApi
 api = GlobeApi.GlobeApi('v1')
 
 "----------Authentication ----------------------"
-oauth = api.oAuth('key-here', 'secret-here')
+#auth = api.oAuth('MeBKgt9nRb8tBginqMTR6gtEaB8BtAb7', '149900c1de054210631719796413d2b0d5d723d8fefed1dda9cac21495a09054')
 
 #get the login url
-print oauth.getLoginUrl()
+#print auth.getLoginUrl()
 
 #get access token
-print oauth.getAccessToken('code-here')
-
+#code = {}
+#code['access_token'] = 'r5GSB885gfapxBpfXj64ruRro6Xs9yq9ehyqbyEUr8qKRCrB4Kxuoe96dCgr58xHd56jbfB7bqeSKrkyoCdgdAgCzgzGMFEqAn7CLMnLBFe84gyIaqr9bU59X4XH9dcMyeiXAeH4ErnbUqa4xBIyMnGLFe6AkxC7kzM5FyedLBCd9kqgC6ybXbSG66bXf8z5ypHeL997CgG4BRuAeqBnCdyb5oUq5qGkhK9oj9sej6E7u4dxyBf4x8qKfp8rXESR';
+#a = auth.getAccessToken(code['access_token'])
+#print a['access_token']
+#token = 'ZlQD_rQWayPbiYbqe0g656-mfoUISbmPg2g4u6m0PzY'
 
 "--------- Send Message ------------------------"
-sms = api.sms('202')
-sms.setRecepient('your-number')
-sms.setMessage('message here')
-print sms.send('token-here')
+#sms = api.sms('3651')
+#sms.setRecepient('9268292535')
+#sms.setCorrelator('[client-correlator]') """Optional"""
+#print sms.send(token)
+
+"------------Sample Receiving Code ------------------------"
+#print sms.getMessages('[registration-ID]')
 
 "--------- Payment Charge Request ---------------"
-payment = api.payment('reference-id')
-payment.setAmount(1)
-payment.setRecepient('your-number')
-print payment.charge('token-here')
+#payment = api.payment('44481000006')
+#payment.setAmount('0.00')
+#payment.setRecepient('9176234236')
+#print payment.charge('X1obYywA-O5GRAEv4vEUrk-MvJF6P3uNkgLQS3roQUw')
